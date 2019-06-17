56ae3c41b2b276eb039c9927c0462abb62c214e9
Silvio Cesar Marqui Limeira <silvio.limeira@mstech.com.br>
2019-06-17 09:46:51 -0300

    improvements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouteReuseStrategy } from "@angular/router";
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+
+import { ErrorHandler } from "@angular/core";
+// import { IonicApp, IonicErrorHandler } from "@ionic/angular";
+import { HomePage } from "./home/home.page";
+import { SocketIoModule, SocketIoConfig } from "ng-socket-io";
+import { FormsModule } from "@angular/forms";
+
+const config: SocketIoConfig = { url: "http://localhost:3001", options: {} };
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    SocketIoModule.forRoot(config)
+  ],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+
+import { NavController } from "@ionic/angular";
+import { Socket } from "ng-socket-io";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage {
+  nickname = "";
 
-  constructor() {}
+  constructor(
+    /*public navCtrl: NavController,*/ public router: Router,
+    private socket: Socket
+  ) {}
 
+  joinChat() {
+    this.socket.connect();
+    this.socket.emit("set-nickname", this.nickname);
+    // this.navCtrl.push('ChatRoomPage', {nickname: this.nickname});
+    this.router.navigate(["/chat-room/", { nickname: this.nickname }]);
+  }
 }
